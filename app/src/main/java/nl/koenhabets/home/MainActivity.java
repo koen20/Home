6cf56869bae78bdc88dc4f25bd333419d4ab6bd5
Koen Habets <koenhabets1@gmail.com>
2017-03-25 14:40:14 +0100

    Added night mode

--- app/src/main/java/nl/koenhabets/home/MainActivity.java
+++ app/src/main/java/nl/koenhabets/home/MainActivity.java
@@ -2,6 +2,7 @@ package nl.koenhabets.home;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
         requestQueue = Volley.newRequestQueue(this);
         textView = (TextView) findViewById(R.id.textView);
         textView2 = (TextView) findViewById(R.id.textView2);
